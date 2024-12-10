b8dfc052a003233d0234dac2b130d30556543674
umar23-zan <umar.zangroups@gmail.com>
2024-12-10 17:31:52 +0530

    working on gig page

--- backend/models/Gig.js
+++ backend/models/Gig.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 const gigSchema = new mongoose.Schema({
   freelancerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  title: String,
-  description: String,
-  price: Number,
-  deliveryTime: Number,
-  category: String,
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  deliveryTime: { type: Number, required: true },
+  category: { type: String, required: true },
   images: [String],
+  
 });
 
 module.exports = mongoose.model('Gig', gigSchema);
